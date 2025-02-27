b2ca8c381b33978ca436a9fa826e16f9cb1d371d
Anton <56392640+AntonBertrand@users.noreply.github.com>
2025-02-27 20:53:10 +0000

    Implemented Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,26 @@ import { useState } from "react";
 import "./App.css";
 import LoadingScreen from "./components/LoadingScreen";
 import "./index.css";
+import Navbar from "./components/Navbar";
+import MobileMenu from "./components/MobileMenu";
 
 function App() {
   const onComplete = () => {
     setIsLoaded(true);
   };
   const [isLoaded, setIsLoaded] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
     <div>
       {!isLoaded && <LoadingScreen onComplete={onComplete} />}
       <div
-        className={`min-h-screen transition-opacity duration-700 ${
+        className={`min-h-screen transition-opacity duration-700 bg-red-500${
           isLoaded ? "opacity-100" : "opacity-0"
         } bg-black text-gray-100`}
       ></div>
+      <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+      <MobileMenu menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
     </div>
   );
 }
