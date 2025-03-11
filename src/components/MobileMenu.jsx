e7b5df8ad647fc85ff6028026a79733c52a44e4a
Anton <56392640+AntonBertrand@users.noreply.github.com>
2025-03-11 16:22:12 +0000

    Added skills section

--- src/components/MobileMenu.jsx
+++ src/components/MobileMenu.jsx
@@ -45,6 +45,17 @@ const MobileMenu = ({ menuOpen, setMenuOpen }) => {
         About
       </a>
       <a
+        href="#skills"
+        onClick={() => setMenuOpen(false)}
+        className={`text-2xl font-semibold text-white my-4 transform transition-transform duration-300 hover:text-gray-200
+            ${
+              menuOpen ? "opacity-100 translate-y-0" : "opacity-0 translate-y-5"
+            }        
+    `}
+      >
+        Skills
+      </a>
+      <a
         href="#projects"
         onClick={() => setMenuOpen(false)}
         className={`text-2xl font-semibold text-white my-4 transform transition-transform duration-300 hover:text-gray-200
