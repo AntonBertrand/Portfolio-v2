35f0f118eb523c19c88527d6a03984b7814e74c2
Anton <56392640+AntonBertrand@users.noreply.github.com>
2025-03-11 21:23:38 +0000

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,7 +9,7 @@ const Navbar = ({ menuOpen, setMenuOpen }) => {
     <nav className="fixed top-0 w-full z-40 bg-[rgba(10, 10, 10 , 0.8)] backdrop-blur-lg border-b border-white/10 shadow-lg">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between items-center h-16">
-          <a href="" className="text-white text-2xl font-mono font-bold">
+          <a href="" className="text-white text-2xl  ">
             Anton Bertrand
           </a>
 
